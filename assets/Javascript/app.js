2f903d1905671df17bfa5affe2a39bcc954f00e3
Jessica Cervantes <J1123C@gmail.com>
2018-08-20 22:15:37 -0700

    HTTPS

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -7,7 +7,7 @@ var gifToggle = false;
 function displayGifyInfo() {
 
     var gif = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=HIfJKXEPkOb2EzAX2ALJrGuCkpXyw8PU&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=HIfJKXEPkOb2EzAX2ALJrGuCkpXyw8PU&limit=10";
 
     $.ajax({
         url: queryURL,
